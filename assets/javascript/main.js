b008ef33e9b63e7150a040e217538d668e28d59c
Garrett <gjblythe@gmail.com>
2018-09-16 12:23:15 -0700

    with input clear

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,84 +10,98 @@ firebase.initializeApp(config);
 
 database = firebase.database();
 
-var name = "";
-var destination = "";
-var firstTrainTime = "";
-var frequency = "";
-
-
 $(document).ready(function() {
-    console.log("ready");
-    clock();
-});
+  console.log("ready");
+  clock();
 
-function clock(){
+  function clock() {
     setTimeout(clock, 1000);
-    $('#currentTime').html(moment().format('LTS'));
-};
+    $("#currentTime").html(moment().format("LTS"));
+  }
 
-$("button").click(function(event){
-    event.preventDefault();
-    name = $('#trainName').val().trim();
-    destination = $('#destination').val().trim();
-    firstTrainTime = $('#firstTrain').val().trim();
-    frequency = $('#frequency').val().trim();
+  $("button").click(function(event) {
+    var name = $("#trainName")
+      .val()
+      .trim();
+    var destination = $("#destination")
+      .val()
+      .trim();
+    var firstTrainTime = $("#firstTrain")
+      .val()
+      .trim();
+    var frequency = $("#frequency")
+      .val()
+      .trim();
 
-if (firstTrainTime === ""){
-    firstTrainTime = moment().format('hh:mm');
-} else {
-    firstTrainTime = moment(firstTrainTime, 'hh:mm').format('hh:mm')
-};
+    if (firstTrainTime === "") {
+      firstTrainTime = moment().format("hh:mm");
+    } else {
+      firstTrainTime = moment(firstTrainTime, "hh:mm").format("hh:mm");
+    }
 
-if (frequency === null) {
-    console.error("Enter Frequency");
-    return;
-} else {
-    frequency = moment(frequency, "mm:ss").format("mm:ss");
-};
-database.ref().push({
-    name: name,
-    destination: destination,
-    firstTrainTime: firstTrainTime,
-    frequency: frequency,
-    dateAdded: moment().format("X")
-})
+    if (frequency === null) {
+      console.error("Enter Frequency");
+      return;
+    } else {
+      frequency = moment(frequency, "mm:ss").format("mm:ss");
+    }
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTrainTime: firstTrainTime,
+      frequency: frequency,
+      dateAdded: moment().format("X")
+    });
+    name = "";
+    destination = "";
+    firstTrainTime = "";
+    frequency = "";
 
-})
+    $("#trainName").val(name);
+    $("#destination").val(destination);
+    $("#firstTrain").val(firstTrainTime);
+    $("#frequency").val(frequency);
+    return false;
+  });
 
-database.ref().on("child_added", function(snapshot) {
-    
+  database.ref().on("child_added", function(snapshot) {
     // Log everything that's coming out of snapshot
     trainName = snapshot.val().name;
     trainDest = snapshot.val().destination;
     firstTrain = snapshot.val().firstTrainTime;
     freq = snapshot.val().frequency;
-    
-    
+
     console.log(trainName);
     console.log(trainDest);
     console.log(firstTrain);
     console.log(freq);
-    
-    $('#trains').append('<tr><td><i class="fas fa-subway"></i></td><td id="name">' + trainName + '</td><td id="dest">' 
-    + trainDest + '</td><td id="firstTrain">' + firstTrain + '</td><td id="freq">'
-    + freq + '</td><tr>')
-    timeToNext();
-});
 
-// database.ref().orderByChild(INSERT LOGIC NEXT TRAIN).limitToLast(Look INTO THIS).on("child_added", function(snapshot){
-    // $('#trains').append('<tr><td id="name">' + trainName + '</td><td id="dest">' 
-    // + trainDest + '</td><td id="firstTrain">' + firstTrain + '</td><td id="freq">'
-    // + freq + '</td><tr>'))
-    
-    
-function timeToNext (){
-    var time = moment().format("hh:mm");
+    $("#trains").append(
+      '<tr><td><i class="fas fa-subway"></i></td><td id="name">' +
+        trainName +
+        '</td><td id="dest">' +
+        trainDest +
+        '</td><td id="firstTrain">' +
+        firstTrain +
+        '</td><td id="freq">' +
+        freq +
+        "</td><tr>"
+    );
+  });
+
+  // database.ref().orderByChild(INSERT LOGIC NEXT TRAIN).limitToLast(Look INTO THIS).on("child_added", function(snapshot){
+  // $('#trains').append('<tr><td id="name">' + trainName + '</td><td id="dest">'
+  // + trainDest + '</td><td id="firstTrain">' + firstTrain + '</td><td id="freq">'
+  // + freq + '</td><tr>'))
+
+  function timeToNext() {
+    var time = moment.utc().format("X");
+
     console.log("time ", time);
 
     // this is coming back as an object
 
-    var untilNext = moment(time).subtract(firstTrain);
+    var untilNext = moment(time).subtract(firstTrain.UTC);
     console.log("untilNext ", untilNext);
 
     var modulus = untilNext % freq;
@@ -98,14 +112,12 @@ function timeToNext (){
 
     var nextTrain = remaider + time;
     console.log(nextTrain);
-    $('#next').text(nextTrain)
-}
-    
+    $("#next").text(nextTrain);
+  }
 
-
-
-
-
-
-
-
+  // function removeTrain(){
+  //     $('.fas fa-subway').click(function(){
+  //         ref.child(key).remove(this);
+  //     })
+  // }
+});
